3f72c85d728581ae7e30b36489b0a2855490acfb
Matej Halek <matej.halek@infis.cz>
2024-09-18 13:19:30 +0200

    Calculation

--- lesson_1.java
+++ lesson_1.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 public class lesson_1 {
     public static void main(String[] args) {    //main
-        System.out.println("Hello World");      //sout
+//        int vek = 0;
+//        double PI = 3.14159d;
+//        boolean isTrue = true;
+//        char bigA = 'A';
+//        String name = "Jmeno";
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Napiš slovo Karel");
+//        String userInout = sc.next();
+
+//        Cv. 1
+
+//        int a = 5;
+//        int b = 10;
+//        int temp = 0;
+//        temp = a;
+//        a = b;
+//        b = temp;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        cv 2
+
+
+/*        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Zapiš první číslo");
+        int firstNumber = sc.nextInt();
+        System.out.println("Zapiš druhé číslo");
+        int secondNumber = sc.nextInt();
+
+        int plusResult = firstNumber + secondNumber;
+        int minusResult = firstNumber - secondNumber;
+        int timesResult = firstNumber * secondNumber;
+        int devideResult = firstNumber / secondNumber;
+        System.out.println("Výsledky");
+        System.out.println(plusResult);
+        System.out.println(minusResult);
+        System.out.println(timesResult);
+        System.out.println(devideResult);*/
+
+
+
+
+
+
     }
 }
